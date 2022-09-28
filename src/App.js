7539c224cd884464f9d7a791a7a36121397485db
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-09-28 21:31:30 +0600

    fake data ready

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [sports, setSports] = useState([]);
+
+  useEffect(() => {
+    fetch('fakedb.json')
+      .then(res => res.json())
+      .then(data => setSports(data));
+  }, [])
+
+  console.log(sports);
+
+
   return (
-    <div className='container'>
-      <div>
-        <p>main side.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni ducimus perspiciatis accusamus, fugiat tempore ratione commodi necessitatibus laborum eveniet incidunt molestiae placeat explicabo! Corporis, hic consequuntur sit, velit non a quasi modi quia esse delectus nemo commodi nulla mollitia ipsum cupiditate consectetur voluptatibus assumenda blanditiis quod alias saepe in! Hic!</p>
+    <div className='main-container'>
+
+      <div className='left-side'>
+
+        <div>
+          <h1 className='club-title'>Desi Traditional Sports Club</h1>
+        </div>
+
+
+
       </div>
 
-      <div>
+      <div className='right-side'>
         <p>Functional side</p>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ad quidem repellendus iure eum veritatis sint. Saepe similique aliquid deleniti corporis consequatur qui alias iusto in sequi ipsum! Eaque voluptas aliquid nobis rerum quos explicabo nostrum itaque placeat expedita architecto doloribus cum molestiae ex ad, ipsum recusandae, ut tempore pariatur.</p>
 
