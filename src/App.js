e9e00f771d3ba6209e751717fe0c74a6ee831fdd
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-09-28 21:04:37 +0600

    project initial setup done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div>
+        <p>main side.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni ducimus perspiciatis accusamus, fugiat tempore ratione commodi necessitatibus laborum eveniet incidunt molestiae placeat explicabo! Corporis, hic consequuntur sit, velit non a quasi modi quia esse delectus nemo commodi nulla mollitia ipsum cupiditate consectetur voluptatibus assumenda blanditiis quod alias saepe in! Hic!</p>
+      </div>
+
+      <div>
+        <p>Functional side</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ad quidem repellendus iure eum veritatis sint. Saepe similique aliquid deleniti corporis consequatur qui alias iusto in sequi ipsum! Eaque voluptas aliquid nobis rerum quos explicabo nostrum itaque placeat expedita architecto doloribus cum molestiae ex ad, ipsum recusandae, ut tempore pariatur.</p>
+
+      </div>
+
     </div>
   );
 }
